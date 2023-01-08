372bcd9ead04761e8803864a40caf6a62b806d3a
andremp12 <ampputra87@gmail.com>
2023-01-08 21:12:32 +0700

    PBO_Teori_2101092029

---
+++ PBO_TEORI_2101092029/src/Modul_3/TheTree.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_3;
+
+/**
+ *
+ * @author user
+ */
+public class TheTree {
+    public static void main(String[] args){
+        System.out.println("I think that I shall never see,\n" + "a poem as lovely as a tree.\n" +
+                "A tree whose hungry mouth is pressed\n" + "Against the Earth’s sweet flowing breast.");
+    }
+}
