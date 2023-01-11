a4a6dda3adba98113c922f6361e3a4da0c433755
andremp12 <ampputra87@gmail.com>
2023-01-11 12:02:14 +0700

    PBO_Teori_2101092029

---
+++ PBO_TEORI_2101092029/src/Modul_8/Latihan_1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul_8;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan_1 {
+    public static void main(String[] args){
+        int i;
+        
+        for(i=1;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
